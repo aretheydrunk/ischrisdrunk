2bbc98488212d3ce29b8aa71e1f4a3463f461d6a
Tudor-Dan Ravoiu <tudor2004@yahoo.com>
2019-07-11 19:17:41 +0200

    added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule} from '@angular/material';
+import {MatDatepickerModule, MatFormFieldModule, MatCardModule, MatNativeDateModule, MatInputModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
@@ -20,6 +20,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
         AppRoutingModule,
         MatButtonModule,
         MatCheckboxModule,
+        MatCardModule,
         BrowserModule, MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, MatInputModule, BrowserAnimationsModule
     ],
     providers: [],
